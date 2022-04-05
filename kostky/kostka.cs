99b5fbf59f4d9f4a914d4b944291aaa97555f76e
Patrik Sigl <Sigl.Patrik@stud.vassboskovice.cz>
2022-04-05 11:35:09 +0200

    Přidejte soubory projektu.

---
+++ kostky/kostka.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace kostky
+{
+    class kostka
+    {
+        private int hodnota;
+
+        public int Hodnota { get => hodnota; }
+
+        public void Hod()
+        {
+            Random random = new Random();
+            hodnota = random.Next(1, 7);
+        }
+
+        public kostka()
+        {
+            Hod();
+        }
+            
+
+    }
+}
